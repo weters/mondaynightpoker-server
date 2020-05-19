5b2433123f3c08ef694f28dd7a5840b92fe30b5f
Tom Peters <weters@me.com>
2020-05-18 20:19:24 -0400

    Can block players from participating

--- pkg/table/player_table.go
+++ pkg/table/player_table.go
@@ -16,6 +16,7 @@ players_tables.can_restart,
 players_tables.can_terminate,
 players_tables.balance,
 players_tables.active,
+players_tables.is_blocked,
 players_tables.created,
 players_tables.updated`
 
@@ -31,6 +32,7 @@ type PlayerTable struct {
 	CanTerminate bool      `json:"canTerminate"`
 	Balance      int       `json:"balance"`
 	Active       bool      `json:"active"`
+	IsBlocked    bool      `json:"isBlocked"`
 	Created      time.Time `json:"created"`
 	Updated      time.Time `json:"updated"`
 }
@@ -41,7 +43,7 @@ func getPlayerTableByRow(row db.Scanner) (*PlayerTable, error) {
 
 	if err := row.Scan(&p.ID, &p.Email, &p.DisplayName, &p.IsSiteAdmin, &p.passwordHash, &p.Created, &p.Updated,
 		&pt.ID, &pt.PlayerID, &pt.TableUUID, &pt.IsTableAdmin, &pt.CanStart, &pt.CanRestart, &pt.CanTerminate,
-		&pt.Balance, &pt.Active, &pt.Created, &pt.Updated); err != nil {
+		&pt.Balance, &pt.Active, &pt.IsBlocked, &pt.Created, &pt.Updated); err != nil {
 		return nil, err
 	}
 
@@ -68,31 +70,19 @@ func (p *PlayerTable) AdjustBalance(ctx context.Context, byAmount int, reason st
 	return nil
 }
 
-// SetActive sets the active state for the player table in the database
-func (p *PlayerTable) SetActive(ctx context.Context, active bool) error {
-	const query = `
-UPDATE players_tables
-SET active = $1, updated = (NOW() AT TIME ZONE 'UTC')
-WHERE id = $2`
-	execContext, err := db.Instance().ExecContext(ctx, query, active, p.ID)
-	if err != nil {
-		return err
-	}
-
-	if ra, _ := execContext.RowsAffected(); ra > 0 {
-		p.Active = active
-	}
-
-	return nil
-}
-
 // Save will save non-balance values
 func (p *PlayerTable) Save(ctx context.Context) error {
 	const query = `
 UPDATE players_tables
-SET is_table_admin = $1, can_start = $2, can_restart = $3, can_terminate = $4, updated = (NOW() AT TIME ZONE 'utc')
-WHERE id = $5`
+SET active = $1, is_table_admin = $2, can_start = $3, can_restart = $4, can_terminate = $5, is_blocked = $6, updated = (NOW() AT TIME ZONE 'utc')
+WHERE id = $7`
 
-	_, err := db.Instance().ExecContext(ctx, query, p.IsTableAdmin, p.CanStart, p.CanRestart, p.CanTerminate, p.ID)
+	_, err := db.Instance().ExecContext(ctx, query, p.Active, p.IsTableAdmin, p.CanStart, p.CanRestart, p.CanTerminate, p.IsBlocked, p.ID)
 	return err
+}
+
+// IsPlaying returns true if the player should be dealt in the next hand
+// This will return false if player is marked as not active, or they are blocked (by table admin)
+func (p *PlayerTable) IsPlaying() bool {
+	return !p.IsBlocked && p.Active
 }
